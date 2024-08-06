bf1510814e7cad9c79c4e32059900c6d7e34f985
Favourr <ofavourite001@gmail.com>
2024-08-06 18:03:47 +0000

    Re-try

--- 0x00-ES6_basic/100-createIteratorObject.js
+++
@@ -1,7 +0,0 @@
-export default function createIteratorObject(report) {
-  const departments = Object.values(report.allEmployees);
-
-  const employees = departments.flat();
-
-  return employees[Symbol.iterator]();
-}
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++
@@ -1,9 +0,0 @@
-export default function iterateThroughObject(reportWithIterator) {
-  const employees = [];
-
-  for (const name of reportWithIterator) {
-    employees.push(name);
-  }
-
-  return employees.join(' | ');
-}
